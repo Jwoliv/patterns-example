6e87dc83a94b13d77a06f24ea599485b54e1cd3b
Lishchuk Bohdan <121406264+Jwoliv@users.noreply.github.com>
2024-06-08 21:03:33 +0300

    add proxy

--- src/main/java/org/example/structural/proxy/realization/ProxyItem.java
+++ src/main/java/org/example/structural/proxy/realization/ProxyItem.java
@@ -1,7 +1,7 @@
 package org.example.structural.proxy.realization;
 
 public class ProxyItem implements ItemI {
-    private final ObjectItem objectItem = new ObjectItem();
+    private final ItemI objectItem = new ObjectItem();
 
     @Override
     public void invoke() {
